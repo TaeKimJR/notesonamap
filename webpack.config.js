6b82d02d5e01db19a81939a9b7b8a34db65c23a1
Tae Kim <tae@talentriot.com>
2016-01-30 01:31:20 -0700

    Added 'nom run build-webpack' command to build client-side app.

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,31 @@
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.jsx',
+    entry: './src/index.jsx',
 
-  output: { path: __dirname, filename: 'bundle.js'},
+    output: { path: __dirname, filename: 'public/bundle.js' },
 
-  module: {
-    loaders: [
-      { 
-      	test: /.jsx?$/, 
-      	loader: 'babel-loader', 
-      	exclude: /node_modules/,
-      	query: {
-      		presets: ['es2015', 'react']
-      	} 
-      },
-        {
-            test: /.css$/,
-            loader: ExtractTextPlugin.extract(
-                'style-loader',
-                'css-loader?' +
-                'modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
-            )
-        }
-    ]
-  },
+    module: {
+        loaders: [
+            {
+                test: /.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /.css$/,
+                loader: ExtractTextPlugin.extract(
+                    'style-loader',
+                    'css-loader?' +
+                    'modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+                )
+            }
+        ]
+    },
     plugins: [
-        new ExtractTextPlugin('css/app.css')
+        new ExtractTextPlugin('public/css/app.css')
     ]
-}
+};
