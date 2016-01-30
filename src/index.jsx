5adf93eca3360effdd80f255f346b48945d534b1
Tae Kim <tae@talentriot.com>
2016-01-30 01:21:42 -0700

    Added ESLint

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,9 @@ import CSSModules from 'react-css-modules';
 import styles from './index.css';
 
 class HelloWorld extends React.Component {
-	render() {
-		return <h1 styleName="app">Hello World</h1>
-	}
+    render() {
+        return <h1 styleName="app">Hello World</h1>;
+    }
 }
 
 const App = CSSModules(HelloWorld, styles);
